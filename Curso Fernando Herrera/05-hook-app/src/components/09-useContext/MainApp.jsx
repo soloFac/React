f821c079fef016ab1c6bd9db2b395fb4f75fda54
soloFac <francopldr@gmail.com>
2022-12-25 23:46:34 -0300

    before install react-router-dom

---
+++ Curso Fernando Herrera/05-hook-app/src/components/09-useContext/MainApp.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const MainApp = () => {
+  return (
+    <>
+      <h1>MainApp</h1>
+      <hr />
+    </>
+  )
+}
+
+export default MainApp
--- Curso Fernando Herrera/05-hook-app/src/index.js
+++ Curso Fernando Herrera/05-hook-app/src/index.js
@@ -3,18 +3,19 @@ import ReactDOM from 'react-dom'
 // import { FormWithCustomHook } from './components/02-useEffect/FormWithCustomHook';
 // import { MultipleCustomHooks } from './components/03-examples/MultipleCustomHooks';
 // import FocusScreen from './components/04-useRef/FocusScreen'
-import RealExampleRef from './components/04-useRef/RealExampleRef'
-import { Padre } from './components/07-tarea-memo/Padre'
+// import RealExampleRef from './components/04-useRef/RealExampleRef'
+// import { Padre } from './components/07-tarea-memo/Padre'
 
 import './components/08-useReducer/intro-reducer'
 // import { CounterApp } from './components/01-useState/CounterApp';
 // import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook';
 // import { SimpleForm } from './components/02-useEffect/SimpleForm';
 // import { HookApp } from './HookApp';
-import TodoApp from './components/08-useReducer/TodoApp'
+// import TodoApp from './components/08-useReducer/TodoApp'
+import MainApp from './components/09-useContext/MainApp'
 
 ReactDOM.render(
-  <TodoApp />,
+  <MainApp />,
   document.getElementById('root')
 )
 
