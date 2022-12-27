115ff5693e9dcbedee984ac42d81d3e2debb6eed
soloFac <francopldr@gmail.com>
2022-12-27 19:59:27 -0300

    SearchComponent

--- Curso Fernando Herrera/07-heroes-spa/src/heroes/pages/SearchPage.jsx
+++ Curso Fernando Herrera/07-heroes-spa/src/heroes/pages/SearchPage.jsx
@@ -1,7 +1,27 @@
 import React from 'react'
+import { useLocation, useNavigate } from 'react-router-dom'
+import queryString from 'query-string'
+
 import { HeroCard } from '../components'
+import useForm from '../../hooks/useForm'
 
 export const SearchPage = () => {
+  const navigate = useNavigate()
+  const location = useLocation()
+
+  const { q = '' } = queryString.parse(location.search)
+
+  const { searchText, onInputChange } = useForm({
+    searchText: ''
+  })
+
+  const onSearchSubmit = (e) => {
+    e.preventDefault()
+    if (searchText.trim().length <= 1) return
+
+    navigate(`?q=${searchText}`)
+  }
+
   return (
     <>
       <h1>Search</h1>
@@ -12,13 +32,15 @@ export const SearchPage = () => {
           <h4>Searching</h4>
           <hr />
 
-          <form>
+          <form onSubmit={onSearchSubmit}>
             <input
               type='text'
               placeholder='Search a hero'
               className='form-control'
-              name='serachText'
+              name='searchText'
               autoComplete='off'
+              value={searchText}
+              onChange={onInputChange}
             />
 
             <button className='btn btn-outline-primary mt-1'>
@@ -36,7 +58,7 @@ export const SearchPage = () => {
           </div>
 
           <div className='alert alert-danger'>
-            No hero with <b>ABC</b>
+            No hero with <b>{q}</b>
           </div>
 
           {/* <HeroCard /> */}
