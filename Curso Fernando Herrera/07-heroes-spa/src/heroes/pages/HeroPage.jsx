71895820188ab973976b862ae61aa1f4d09e22b6
soloFac <francopldr@gmail.com>
2022-12-27 18:41:05 -0300

    Estilo del componente HeroScreen

--- Curso Fernando Herrera/07-heroes-spa/src/heroes/pages/HeroPage.jsx
+++ Curso Fernando Herrera/07-heroes-spa/src/heroes/pages/HeroPage.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Navigate, useParams } from 'react-router-dom'
+import { Navigate, useNavigate, useParams } from 'react-router-dom'
 import { getHeroById } from '../helpers'
 
 export const HeroPage = () => {
@@ -7,13 +7,42 @@ export const HeroPage = () => {
 
   const hero = getHeroById(id)
 
+  const navigate = useNavigate()
+  const onNavigateBack = () => {
+    navigate(-1)
+  }
+
   if (!hero) {
     return <Navigate to='/marvel' />
   }
 
   return (
-    <>
-      <h1>{ hero?.superhero }</h1>
-    </>
+    <div className='row mt-5'>
+      <div className='col-4'>
+        <img
+          src={`/assets/heroes/${id}.jpg`}
+          alt={hero.superhero}
+          className='img-thumbnail'
+        />
+      </div>
+      <div className='col-8'>
+        <h3>{hero.superhero}</h3>
+        <ul className='list-group list-group-flush'>
+          <li className='list-group-item'><b>Alter ego: </b> {hero.alterEgo}</li>
+          <li className='list-group-item'><b>Publisher: </b> {hero.publisher}</li>
+          <li className='list-group-item'><b>First appearance: </b> {hero.firstAppearance}</li>
+        </ul>
+
+        <h5 className='mt-3'>Characters</h5>
+        <p>{hero.characters}</p>
+
+        <button
+          className='btn btn-outline-primary'
+          onClick={onNavigateBack}
+        >
+          Regresar
+        </button>
+      </div>
+    </div>
   )
 }
