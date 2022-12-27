3c4a3b7ad589472ce2d2ab535b47f77ba3dee026
soloFac <francopldr@gmail.com>
2022-12-27 18:24:55 -0300

    Leer argumentos por URL

--- Curso Fernando Herrera/07-heroes-spa/src/heroes/pages/HeroPage.jsx
+++ Curso Fernando Herrera/07-heroes-spa/src/heroes/pages/HeroPage.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { Navigate, useParams } from 'react-router-dom'
+import { getHeroById } from '../helpers'
 
 export const HeroPage = () => {
+  const { id } = useParams()
+
+  const hero = getHeroById(id)
+
+  if (!hero) {
+    return <Navigate to='/marvel' />
+  }
+
   return (
     <>
-      <h1>Hero</h1>
+      <h1>{ hero?.superhero }</h1>
     </>
   )
 }
