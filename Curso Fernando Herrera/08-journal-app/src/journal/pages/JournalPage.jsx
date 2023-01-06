89d4963587dd4caaa780912c9a10448c83a30c3f
soloFac <francopldr@gmail.com>
2023-01-06 17:23:24 -0300

    NothingSelectedView - with wrong styles

--- Curso Fernando Herrera/08-journal-app/src/journal/pages/JournalPage.jsx
+++ Curso Fernando Herrera/08-journal-app/src/journal/pages/JournalPage.jsx
@@ -1,13 +1,13 @@
-import { Typography } from '@mui/material'
 import React from 'react'
 import { JournalLayout } from '../layout/JournalLayout'
+import { NothingSelectedView } from '../views'
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
-      <Typography >Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab repudiandae dolor rerum esse sint iusto, non sequi explicabo quia deserunt consequuntur assumenda recusandae ipsum aliquam, fugiat facilis odit quidem minima.</Typography>
+      {/* <Typography >Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab repudiandae dolor rerum esse sint iusto, non sequi explicabo quia deserunt consequuntur assumenda recusandae ipsum aliquam, fugiat facilis odit quidem minima.</Typography> */}
 
-      {/* NothingSelected */}
+      <NothingSelectedView />
       {/* NoteView */}
 
     </JournalLayout>
