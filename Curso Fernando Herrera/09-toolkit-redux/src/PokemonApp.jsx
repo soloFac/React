a2ba86f60e9505b3efe3216a8cc94c621d61b120
soloFac <francopldr@gmail.com>
2023-01-07 08:21:58 -0300

    Mostrar los pokemons paginadamente

--- Curso Fernando Herrera/09-toolkit-redux/src/PokemonApp.jsx
+++ Curso Fernando Herrera/09-toolkit-redux/src/PokemonApp.jsx
@@ -1,9 +1,12 @@
 import React, { useEffect } from 'react'
-import { useDispatch } from 'react-redux'
-import { getPokemon } from './store/slices/pokemon'
+import { useDispatch, useSelector } from 'react-redux'
+import { getPokemon, pokemonSlice } from './store/slices/pokemon'
 
 export const PokemonApp = () => {
+  const { isLoading, pokemons, page } = useSelector(state => state.pokemons)
   const dispatch = useDispatch()
+  // console.log(isLoading)
+  // console.log(pokemons)
 
   useEffect(() => {
     dispatch(getPokemon())
@@ -14,11 +17,21 @@ export const PokemonApp = () => {
       <h1>PokemonApp</h1>
       <hr />
 
+      <span>Loading: { isLoading ? 'True' : 'False'} </span>
+
       <ul>
-        <li>Hola</li>
-        <li>Hola</li>
-        <li>Hola</li>
+        {pokemons.map(({ name }) => (
+          <a key={name}><li>{name}</li></a>
+        ))}
+
       </ul>
+
+      <button
+        disabled={isLoading}
+        onClick={() => dispatch(getPokemon(page))}
+      >
+        Next
+      </button>
     </>
   )
 }
