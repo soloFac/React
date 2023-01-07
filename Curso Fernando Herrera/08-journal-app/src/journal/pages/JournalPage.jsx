a2250bd8adf43f9eaf68d4ed680b397583302c4d
soloFac <francopldr@gmail.com>
2023-01-06 22:44:50 -0300

    ImageList

--- Curso Fernando Herrera/08-journal-app/src/journal/pages/JournalPage.jsx
+++ Curso Fernando Herrera/08-journal-app/src/journal/pages/JournalPage.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import { JournalLayout } from '../layout/JournalLayout'
-import { NothingSelectedView } from '../views'
+import { NoteView, NothingSelectedView } from '../views'
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
       {/* <Typography >Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab repudiandae dolor rerum esse sint iusto, non sequi explicabo quia deserunt consequuntur assumenda recusandae ipsum aliquam, fugiat facilis odit quidem minima.</Typography> */}
 
-      <NothingSelectedView />
-      {/* NoteView */}
+      {/* <NothingSelectedView /> */}
+      <NoteView />
 
     </JournalLayout>
   )
