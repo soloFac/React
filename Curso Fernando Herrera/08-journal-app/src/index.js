5972e56c5a9e14f0288fa3833032179c34cf052c
soloFac <francopldr@gmail.com>
2023-01-08 15:14:51 -0300

    Comienzo de Introducción a Redux y autenticación en Firebase - JournalApp

--- Curso Fernando Herrera/08-journal-app/src/index.js
+++ Curso Fernando Herrera/08-journal-app/src/index.js
@@ -5,13 +5,17 @@ import reportWebVitals from './reportWebVitals'
 import { JournalApp } from './JournalApp'
 import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from '@emotion/react'
+import { Provider } from 'react-redux'
+import { store } from './store'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <JournalApp />
-    </BrowserRouter>
+    <Provider store={ store }>
+      <BrowserRouter>
+        <JournalApp />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 )
 
