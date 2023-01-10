17d7802a57b3b5590b4cd4993ba4775443605dc6
soloFac <francopldr@gmail.com>
2023-01-10 12:46:23 -0300

    Checking Authentication

--- Curso Fernando Herrera/08-journal-app/src/router/AppRouter.jsx
+++ Curso Fernando Herrera/08-journal-app/src/router/AppRouter.jsx
@@ -2,8 +2,16 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import { AuthRoutes } from '../auth/routes/AuthRoutes'
 import { JournalPage } from '../journal/pages/JournalPage'
+import { useSelector } from 'react-redux'
+import { CheckingAuth } from '../ui'
 
 export const AppRouter = () => {
+  const { status } = useSelector(state => state.auth)
+
+  if (status === 'checking') {
+    return <CheckingAuth />
+  }
+
   return (
     <Routes>
 
