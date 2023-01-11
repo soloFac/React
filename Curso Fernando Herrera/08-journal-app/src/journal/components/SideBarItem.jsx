9359ddbe94ba69ff17406beb74e63e92767a0b56
soloFac <francopldr@gmail.com>
2023-01-11 11:59:22 -0300

    Activar una nota

--- Curso Fernando Herrera/08-journal-app/src/journal/components/SideBarItem.jsx
+++ Curso Fernando Herrera/08-journal-app/src/journal/components/SideBarItem.jsx
@@ -1,21 +1,33 @@
 import { TurnedInNot } from '@mui/icons-material'
 import { Grid, ListItem, ListItemButton, ListItemIcon, ListItemText } from '@mui/material'
 import React, { useMemo } from 'react'
+import { useDispatch } from 'react-redux'
+import { setActiveNote } from '../../store/journal/journalSlice'
 
-export const SideBarItem = ({ title, body, id }) => {
+export const SideBarItem = ({ title = '', body, id, date, imageUrls = [] }) => {
+  const dispatch = useDispatch()
+
   const newTitle = useMemo(() => {
     return title.length > 17
       ? title.substring(0, 17) + '...'
       : title
   }, [title])
 
+  const activateNote = () => {
+    dispatch(setActiveNote({ id, title, date, body, imageUrls }))
+  }
+
   return (
-    <ListItem disablePadding>
+    <ListItem
+      disablePadding
+      onClick={activateNote}
+    >
       <ListItemButton>
         <ListItemIcon>
           <TurnedInNot />
         </ListItemIcon>
-        <Grid container>
+        <Grid container
+        >
           <ListItemText primary={newTitle} />
           <ListItemText secondary={body} />
         </Grid>
