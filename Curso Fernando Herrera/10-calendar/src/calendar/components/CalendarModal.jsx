28385e19a7e9106685b0d52a9b72316b4132b100
soloFac <francopldr@gmail.com>
2023-01-14 17:41:01 -0300

    Validaciones del formulario

--- Curso Fernando Herrera/10-calendar/src/calendar/components/CalendarModal.jsx
+++ Curso Fernando Herrera/10-calendar/src/calendar/components/CalendarModal.jsx
@@ -1,10 +1,12 @@
 import { addHours, differenceInSeconds } from 'date-fns/esm'
-import React, { useState } from 'react'
+import React, { useMemo, useState } from 'react'
 
 import Modal from 'react-modal'
 import DatePicker, { registerLocale } from 'react-datepicker'
 import es from 'date-fns/locale/es'
 import 'react-datepicker/dist/react-datepicker.css'
+import Swal from 'sweetalert2'
+import 'sweetalert2/dist/sweetalert2.min.css'
 
 registerLocale('es', es)
 
@@ -23,6 +25,7 @@ Modal.setAppElement('#root')
 
 export const CalendarModal = () => {
   const [isOpen, setIsOpen] = useState(true)
+  const [formSubmitted, setFormSubmitted] = useState(false)
 
   const [formValues, setFormValues] = useState({
     title: 'Franco',
@@ -30,6 +33,14 @@ export const CalendarModal = () => {
     start: new Date(),
     end: addHours(new Date(), 2)
   })
+
+  const titleClass = useMemo(() => {
+    if (!formSubmitted) return ''
+
+    return (formValues.title.length > 0)
+      ? ''
+      : 'is-invalid'
+  }, [formValues.title, formSubmitted])
 
   const onCloseModal = () => {
     console.log('cerrando modal')
@@ -53,12 +64,14 @@ export const CalendarModal = () => {
 
   const onSubmit = (event) => {
     event.preventDefault()
+    setFormSubmitted(true)
+
     const { start, end } = formValues
 
     const difference = differenceInSeconds(end, start)
 
     if (isNaN(difference) || difference <= 0) {
-      console.log('Error en fechas')
+      Swal.fire('Fechas incorrectas', 'Revisar las fechas ingresadas', 'error')
       return
     }
 
@@ -119,7 +132,7 @@ export const CalendarModal = () => {
           <label>Titulo y notas</label>
           <input
             type='text'
-            className='form-control'
+            className={`form-control ${titleClass}`}
             placeholder='Título del evento'
             name='title'
             autoComplete='off'
