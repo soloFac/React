cdf604b17e842705ff683870fa9e6e945485e0a3
soloFac <francopldr@gmail.com>
2023-01-14 01:10:45 -0300

    Configuraciones adicionales al calendario - Personalizar el cuadro de evento

--- Curso Fernando Herrera/10-calendar/src/calendar/pages/CalendarPage.jsx
+++ Curso Fernando Herrera/10-calendar/src/calendar/pages/CalendarPage.jsx
@@ -1,24 +1,11 @@
 import React from 'react'
 
-import { Calendar, dateFnsLocalizer } from 'react-big-calendar'
+import { Calendar } from 'react-big-calendar'
 import 'react-big-calendar/lib/css/react-big-calendar.css'
 
-import enUS from 'date-fns/locale/en-US'
-
-import { Navbar } from '../'
-import { addHours, format, parse, startOfWeek, getDay } from 'date-fns'
-
-const locales = {
-  'en-US': enUS
-}
-
-const localizer = dateFnsLocalizer({
-  format,
-  parse,
-  startOfWeek,
-  getDay,
-  locales
-})
+import { addHours } from 'date-fns'
+import { CalendarEvent, Navbar } from '../'
+import { localizer, getMessagesES } from '../../helpers'
 
 const events = [{
   title: 'Cumpleaños del Jefe',
@@ -33,16 +20,34 @@ const events = [{
 }]
 
 export const CalendarPage = () => {
+  const eventStyleGetter = (event, start, end, isSelected) => {
+    const style = {
+      backgroundColor: '#347CF7',
+      borderRadius: '0px',
+      opacity: 0.8,
+      color: 'white'
+    }
+
+    return { style }
+  }
+
   return (
     <>
       <Navbar />
 
       <Calendar
+        culture='es'
         localizer={localizer}
         events={events}
         startAccessor='start'
         endAccessor='end'
         style={{ height: 'calc( 100vh - 80px )' }}
+        messages={getMessagesES()}
+        eventPropGetter={eventStyleGetter}
+        components={{
+          // Solo estoy llamando la referencia del CalendarEvent
+          event: CalendarEvent
+        }}
       />
     </>
   )
