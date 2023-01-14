a520e01966c88acf0f85c3c1c6bdc12fee091b9d
soloFac <francopldr@gmail.com>
2023-01-14 00:40:18 -0300

    React Big Calendar

--- Curso Fernando Herrera/10-calendar/src/calendar/pages/CalendarPage.jsx
+++ Curso Fernando Herrera/10-calendar/src/calendar/pages/CalendarPage.jsx
@@ -1,10 +1,49 @@
 import React from 'react'
+
+import { Calendar, dateFnsLocalizer } from 'react-big-calendar'
+import 'react-big-calendar/lib/css/react-big-calendar.css'
+
+import enUS from 'date-fns/locale/en-US'
+
 import { Navbar } from '../'
+import { addHours, format, parse, startOfWeek, getDay } from 'date-fns'
+
+const locales = {
+  'en-US': enUS
+}
+
+const localizer = dateFnsLocalizer({
+  format,
+  parse,
+  startOfWeek,
+  getDay,
+  locales
+})
+
+const events = [{
+  title: 'Cumpleaños del Jefe',
+  notes: 'Hay que comprar el pastel',
+  start: new Date(),
+  end: addHours(new Date(), 2),
+  bgColor: '#fafafa',
+  user: {
+    _id: '123',
+    name: 'Franco'
+  }
+}]
 
 export const CalendarPage = () => {
   return (
     <>
       <Navbar />
+
+      <Calendar
+        localizer={localizer}
+        events={events}
+        startAccessor='start'
+        endAccessor='end'
+        style={{ height: 'calc( 100vh - 80px )' }}
+      />
     </>
   )
 }
