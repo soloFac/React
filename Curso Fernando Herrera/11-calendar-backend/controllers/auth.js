61a077a812ad9a3cebcd21cd2d6ed6bece169302
soloFac <francopldr@gmail.com>
2023-01-19 17:22:09 -0300

    Login de usuario

--- Curso Fernando Herrera/11-calendar-backend/controllers/auth.js
+++ Curso Fernando Herrera/11-calendar-backend/controllers/auth.js
@@ -1,5 +1,6 @@
 // Para recuperar el intellisense
 const { response } = require('express')
+const bcrypt = require('bcryptjs')
 const Usuario = require('../models/Usuario')
 
 const crearUsuario = async (req, res = response) => {
@@ -17,6 +18,10 @@ const crearUsuario = async (req, res = response) => {
 
     usuario = new Usuario( req.body )
 
+    // Encriptar contraseña
+    const salt = bcrypt.genSaltSync()
+    usuario.password = bcrypt.hashSync(password, salt)
+
     await usuario.save()
 
     res.status(201).json({
@@ -33,9 +38,44 @@ const crearUsuario = async (req, res = response) => {
   }
 }
 
-const loginUsuario = (req, res = response) => {
+const loginUsuario = async (req, res = response) => {
   const { email, password } = req.body
 
+  try {
+    const usuario = await Usuario.findOne({ email })
+
+    if( !usuario ) {
+      return res.status(400).json({
+        ok: false,
+        msg: 'El usuario no existe con ese mail'
+      })
+    }
+
+    // Confirmar los passwords
+    const validPassword = bcrypt.compareSync( password, usuario.password )
+    if ( !validPassword ) {
+      return res.status(400).json({
+        ok: false,
+        msg: 'Password incorrecto'
+      })
+    }
+
+    // Generar nuestro JWT
+
+    res.json({
+      ok: true,
+      uid: usuario.id,
+      name: usuario.name
+    })
+
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: 'Por favor hable con el administrador'
+    })
+  }
+  
   res.status(201).json({
     ok: true,
     msg: 'login',
